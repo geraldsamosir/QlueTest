cd3c1ac8f2c2b499fbce582d0dae2a0e3307d99b
Gerald Halomoan Samosir <geralddeveloper95@gmail.com>
2017-11-11 11:51:18 +0700

    update readme

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  response.send('Hello Qlue!')
 })
 
 app.listen(app.get('port'), function() {
